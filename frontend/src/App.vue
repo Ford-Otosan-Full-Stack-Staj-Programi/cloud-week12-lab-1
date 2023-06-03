1441694439ae6b826b13debbc83acaaca4f38a29
Siraci, Erhan <erhan.siraci@adesso.com.tr>
2023-06-03 11:12:33 +0300

    frontend added

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Hello World!</p>
+  <div v-for="p in data" :key="p.key">
+    <p>{{ p.key }} - {{ p.value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: []
+    }
+  },
+  methods: {
+    getPosts() {
+      fetch('http://localhost/Redis?key=deneme'), { mode: 'no-cors' }
+        .then(response => {
+          console.log(response)
+          return response.json()
+        } )
+        .then(data => this.data = data)
+    }
+  },
+  mounted() {
+    this.getPosts()
+  }
+}
+</script>
