4334a8ed5e687e878c1bb2441e52e992d9ff0efc
Siraci, Erhan <erhan.siraci@adesso.com.tr>
2023-06-03 13:29:00 +0300

    redis connection string moved to env variables

--- backend/RedisService.cs
+++ backend/RedisService.cs
@@ -7,11 +7,31 @@ public class RedisService
     private readonly string _connectionString;
     private readonly string _instanceName;
 
+    public const string REDIS_CONNECTION_STRING = "REDIS_CONNECTION_STRING";
     public RedisService(IConfiguration config)
     {
+        var _connectionStringFromEnvironment = System.Environment.GetEnvironmentVariable(REDIS_CONNECTION_STRING);
+        
         _config = config;
-        _connectionString = _config.GetValue<string>("RedisSettings:ConnectionString");
+
+        if (_connectionStringFromEnvironment != null)
+        {
+            _connectionString = _connectionStringFromEnvironment;
+            Console.WriteLine("Redis Connection String is set from Environment Variable");
+        }
+        else if (_config.GetValue<string>("RedisSettings:ConnectionString") != null)
+        {
+            _connectionString = _config.GetValue<string>("RedisSettings:ConnectionString");
+            Console.WriteLine("Redis Connection String is set from appsettings.json");
+        }
+        else
+        {
+            throw new System.Exception("Redis Connection String is not set");
+        }
+
         _instanceName = _config.GetValue<string>("RedisSettings:InstanceName");
+
+        
         
         Console.WriteLine("Trying to connect Redis using connection string : {0}",_connectionString);
     }
